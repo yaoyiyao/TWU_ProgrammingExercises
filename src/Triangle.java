7816699ccb238025c63612a8f1c174d3db8e645f
710441471@qq.com <710441471@qq.com>
2016-02-25 14:52:43 +0800

    diamond

--- src/Triangle.java
+++ src/Triangle.java
@@ -8,10 +8,11 @@ import java.util.*;
 
 public class Triangle {
     public static void main(String[] args){
-        drawHorizontalLine();
-        drawVerticalLine();
-        drawRightTriangle();
-        drawIsoscelesTriangle();
+//        drawHorizontalLine();
+//        drawVerticalLine();
+//        drawRightTriangle();
+//        drawIsoscelesTriangle();
+        drawDiamond();
     }
 
     //draw a horizontal line
@@ -61,6 +62,33 @@ public class Triangle {
                 System.out.print("*");
             }
             System.out.println();
+        }
+    }
+
+    //draw diamond
+    public static void drawDiamond(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please input the number of diamond triangle u want:");
+        int asteriskNum = input.nextInt();
+        for(int i=0;i<(2*asteriskNum-1);i++) {
+            if (i < asteriskNum) {
+                for (int j = 0; j < (asteriskNum - i); j++) {
+                    System.out.print(" ");
+                }
+                for (int k = 0; k < (2 * i + 1); k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            else{
+                for(int j=0;j<=(i-asteriskNum+1);j++){
+                    System.out.print(" ");
+                }
+                for(int k=0;k<(2*(asteriskNum-i)+3);k++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
         }
     }
 }
